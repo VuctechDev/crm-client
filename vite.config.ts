41f311bef2fca6f23320384ea2b9e80b00917fa0
stefanvucic <stefan.vucic@neotechsolutions.org>
2024-12-20 00:35:06 +0100

    quill2345678

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteCommonjs()],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(["react-quill", "react-quilljs", "quill"])],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
   },
+  // optimizeDeps: {
+  //   esbuildOptions: {
+  //     plugins: [esbuildCommonjs(["react-quill", "react-quilljs", "quill"])],
+  //   },
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Alias @ to src directory
