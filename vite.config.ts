9eaba936e5948a84bc4f638c6348a79230dd4720
stefanvucic <stefan.vucic@neotechsolutions.org>
2024-12-20 00:07:09 +0100

    quill23456

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import vitePluginRequire from "vite-plugin-require";
-
-const aa = vitePluginRequire as unknown as { default: () => any };
+import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), aa.default()],
+  plugins: [react(), commonjs()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Alias @ to src directory
